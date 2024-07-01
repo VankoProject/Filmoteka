c46b07baec96150b924b2cf93e70c8a6ba275ea3
Ivan <ivan.green69@gmail.com>
2024-07-01 19:52:46 +0300

    FK-06 Navigation component dependencies, nav component wrapper and add MainUiState

--- app/src/main/java/com/kliachenko/filmoteka/main/MainUiState.kt
+++ app/src/main/java/com/kliachenko/filmoteka/main/MainUiState.kt
@@ -1,4 +1,20 @@
 package com.kliachenko.filmoteka.main
 
+import android.view.View
+
 interface MainUiState {
+
+    fun updateState(bottomNavigation: View)
+
+    object Main: MainUiState {
+        override fun updateState(bottomNavigation: View) {
+            bottomNavigation.visibility = View.VISIBLE
+        }
+    }
+
+    object Secondary: MainUiState {
+        override fun updateState(bottomNavigation: View) {
+            bottomNavigation.visibility = View.GONE
+        }
+    }
 }
